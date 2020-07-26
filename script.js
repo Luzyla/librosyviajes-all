c5a1e0ac67bdc0bcc0d3cb2e8f736adabde98a82
luzyla <luzyla@gmail.com>
2020-07-26 03:26:35 -0300

    Clase 07: Incorporación de una nueva sección (Feedback) que surge de las respuestas que proporciona el usuario. Función prompt de JavaScript. Código JS dentro de HTML según consigna requerida.

---
+++ script.js
@@ -0,0 +1,10 @@
+      var nombre = prompt('¿Cuál es tu nombre?')
+      var edad = 23
+      var booleana = true
+      var mayor = confirm('¿Tenés más de 18 años?')
+
+      alert('Bienvenid@ ' + nombre)
+
+
+
+
