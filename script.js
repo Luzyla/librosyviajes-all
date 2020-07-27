54dcef9b1c2fa545b7c50092dd7ee74a35bfe830
luzyla <luzyla@gmail.com>
2020-07-26 22:24:58 -0300

    Nuevo H2 sección Redes. Cambio logo en imagen por texto para mejora de efecto. Comentado el script dentro del HTML para evitar los pop-ups.

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-      var nombre = prompt('¿Cuál es tu nombre?')
-      var edad = 23
-      var booleana = true
-      var mayor = confirm('¿Tenés más de 18 años?')
 
-      alert('Bienvenid@ ' + nombre)
 
 
 
